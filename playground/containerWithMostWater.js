a86fafedfd3b8b1fa946f9f3dfc879efea9426ce
544396118 <544396118@qq.com>
2018-11-07 11:57:29 +0800

    feat: add containerWithMostWater

---
+++ playground/containerWithMostWater.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    if(!height || height.length < 2) return 0
+    let maxarea = 0
+    let length = height.length, left = 0, right = length - 1
+    while(left !== right){
+        let area = (right - left) * Math.min(height[left], height[right])
+        if(area > maxarea) {
+            maxarea = area
+        }
+        if(height[left] <= height[right]) {
+            left ++
+        }else{
+            right --
+        }
+    }
+    return maxarea
+};
+console.log(maxArea([1,8,6,2,5,4,8,3,7]))
---
+++ playground/lengthOfLongestSubstring.js
@@ -0,0 +1,24 @@
+var lengthOfLongestSubstring = function(s) {
+    if(!s) return 0
+    let tmp = []
+    let subStrStart = 0
+    let maxSubStrNum = 0
+    for (let i = 0; i < s.length; i ++ ){
+        for(let j = subStrStart; j < s.length; j ++ ){
+            let index = tmp.indexOf(s[j])
+            if(index < 0) {
+                tmp.push(s[j])
+                if(maxSubStrNum < tmp.length) {
+                    maxSubStrNum = tmp.length
+                }
+            }else{
+                subStrStart = subStrStart < s.length ? subStrStart + 1 : s.length
+                tmp = []
+                break
+            }
+        }
+    }
+    return maxSubStrNum
+};
+let str = "ohommdwdsdsaxxxccasdwdad"
+console.info(lengthOfLongestSubstring(str))
