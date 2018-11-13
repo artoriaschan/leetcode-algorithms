4f3fa5683799898a45145dbd3144ed5728c3b4f7
544396118 <544396118@qq.com>
2018-11-13 13:11:35 +0800

    feat: add validParentheses

---
+++ playground/validParentheses.js
@@ -0,0 +1,34 @@
+var isSameTypeParentheses = function(left, right) {
+    let parentheses = "{[()]}"
+    let index = parentheses.indexOf(left)
+    if(index < 0) return false
+    if (right !== parentheses.charAt(5-index)) { 
+        return false
+    }
+    return true
+}
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let parenthesesStack = []
+    let leftParenthesesReg = /[\(\[\{]{1}/
+    let result = true
+    for (let str of s) {
+        if(leftParenthesesReg.test(str)) {
+            parenthesesStack.push(str)
+        }else{
+            let leftParentheses = parenthesesStack.pop()
+            if(!isSameTypeParentheses(leftParentheses, str)) {
+                result = false
+                break
+            }
+        }
+    }
+    if(parenthesesStack.length > 0) result = false
+    return result
+};
+
+let str = "()"
+console.log(isValid(str))
