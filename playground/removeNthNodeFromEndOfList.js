f786067071e69d115bb0524190c03d38758e9637
544396118 <544396118@qq.com>
2018-11-14 15:39:16 +0800

    fix: modify removeNthNodeFromEndOfList and add mergeTwoSortedLists

--- playground/removeNthNodeFromEndOfList.js
+++ playground/removeNthNodeFromEndOfList.js
@@ -15,39 +15,46 @@ function ListNode(val) {
     this.next = null;
 }
 var removeNthFromEnd = function(head, n) {
-
-    // let length = 0
-    // let copy = head
-    // while(head) {
-    //     length ++ 
-    //     head = head.next
-    // }
-    // let removeIndex = length - n
-    // let prevNode = {}
-    // let item = copy
-    // while(removeIndex >= 0){
-    //     if(removeIndex === 0){
-    //         prevNode.next = item.next
-    //         return copy
-    //     }
-    //     prevNode = item
-    //     item = item.next
-    //     removeIndex --
-    // }
-
-    let list = []
+    let length = 0
+    let copy = head
     while(head) {
-        list.push(head.val)
+        length ++ 
         head = head.next
     }
-    let removeIndex = list.length - n
-    list.splice(removeIndex, 1)
-    return list
+    if(length <= 1){
+        return null
+    }
+    let removeIndex = length - n
+    if(removeIndex === 0) {
+        copy = copy.next
+        return copy
+    }
+    let prevNode = copy
+    let pointer = copy
+    while(removeIndex >= 0){
+        if(removeIndex === 0){
+            prevNode.next = pointer.next
+            return copy 
+        }
+        prevNode = pointer
+        pointer = pointer.next
+        removeIndex --
+    }
+
+    // let list = []
+    // while(head) {
+    //     list.push(head.val)
+    //     head = head.next
+    // }
+    // let removeIndex = list.length - n
+    // list.splice(removeIndex, 1)
+    // return list
 };
 let head = new ListNode(1)
 head.next = new ListNode(2)
-head.next.next = new ListNode(3)
-head.next.next.next = new ListNode(4)
-head.next.next.next.next = new ListNode(5)
+// head.next.next = new ListNode(3)
+// head.next.next.next = new ListNode(4)
+// head.next.next.next.next = new ListNode(5)
 
 console.log(JSON.stringify(removeNthFromEnd(head, 2)))
+// console.log(JSON.stringify(removeNthFromEnd(head, 1)))
