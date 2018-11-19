5833a9b0678dd7436799206d25564a4b1e4de8cf
Artorias Chan <544396118@qq.com>
2018-11-19 12:22:44 +0800

    feat: add playground/removeDuplicatesFromSortedArray.js

--- playground/reverseNodesInKGroup.js
+++ playground/reverseNodesInKGroup.js
@@ -13,34 +13,32 @@ function ListNode(val) {
 function debug(obj) {
     console.log(JSON.stringify(obj))
 }
-
-function statisticalLength(head){
-    let length = 0
-    while(head) {
-        length ++
-        head = head.next
-    }
-    return length
-}
 /**
  * @param {ListNode} head
  * @param {number} k
  * @return {ListNode}
  */
 var reverseKGroup = function(head, k) {
-    if(statisticalLength(head) < k) return head
-    head = reverseList(head)
-};
-
-var reverseList =  function (list){
-    var p=list.head,q=null;
-    while(p.next!==null){
-        q=p.next;
-        p.next=q.next;
-        q.next=list.head.next;
-        list.head.next=q;
+    let currentNode = head;
+    if (currentNode == null || k < 0){
+        return head;
     }
-    return list;
+    let count = 0;
+    while (currentNode != null && count < k){ // find the k+1 node
+        currentNode = currentNode.next;
+        count++;
+    }
+    if (count == k){ // if k+1 node is found
+        currentNode = reverseKGroup(currentNode, k); // reverse list with k+1 node as head
+        while (count-- > 0){ // reverse current k-group:
+            let temp = head.next;
+            head.next = currentNode;
+            currentNode = head;
+            head = temp;
+        }
+        head = currentNode;
+    }
+    return head;
 }
 
 let list = new ListNode(1)
@@ -49,4 +47,4 @@ list.next.next = new ListNode(3)
 list.next.next.next = new ListNode(4)
 list.next.next.next.next = new ListNode(5)
 
-reverseKGroup(list)
+debug(reverseKGroup(list, 3));
