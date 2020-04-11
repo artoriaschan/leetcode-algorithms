9a493e6b0851e2819e745395b398d933d69a17ef
artoriaschan <544396118@qq.com>
2020-04-11 21:30:47 +0800

    docs: update 887th daily docs

--- src/daily/887.superEggDrop.js
+++ src/daily/887.superEggDrop.js
@@ -6,7 +6,10 @@
  * 思路:
  *  动态规划 + 二分搜索: 题的标准解法为动态规划
  *    状态定义: dp[i][j] 有i个鸡蛋，j次扔鸡蛋的测得的最多楼层
- *    状态转移方程: dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j] + 1
+ *    状态转移方程: dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1] + 1
+ *      dp[i - 1][j - 1] => 当前楼层鸡蛋碎了
+ *      dp[i][j - 1] => 当前楼层鸡蛋没碎
+ *    一维优化版: dp[i] = dp[i-1] + dp[i] + 1
  */
 // eslint-disable-next-line no-unused-vars
 function superEggDrop(K, N) {
