5c0e874e2f7b34e1a459912f59047e61fcdc1623
artoriaschan <544396118@qq.com>
2020-04-11 21:22:05 +0800

    feat: add 887th question

---
+++ src/daily/887.superEggDrop.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} K
+ * @param {number} N
+ * @return {number}
+ * https://leetcode-cn.com/problems/super-egg-drop/solution/ji-dan-diao-luo-by-leetcode-solution/
+ * 思路:
+ *  动态规划 + 二分搜索: 题的标准解法为动态规划
+ *    状态定义: dp[i][j] 有i个鸡蛋，j次扔鸡蛋的测得的最多楼层
+ *    状态转移方程: dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j] + 1
+ */
+// eslint-disable-next-line no-unused-vars
+function superEggDrop(K, N) {
+  let dp = Array(K + 1).fill(0);
+  let cnt = 0;
+  while (dp[K] < N) {
+    cnt++;
+    for (let i = K; i > 0; i--) {
+      dp[i] = dp[i - 1] + dp[i] + 1;
+    }
+  }
+  return cnt;
+}
